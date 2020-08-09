84a017f46bf8db9ee38cfba82b5781c5a6e7a686
dxi02-at-329541981040 <daniverson@hotmail.com>
2020-08-09 16:48:46 -0400

    Combining python starts

---
+++ wsl2/Functions/new-python3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+print("Hellow, World!")
+
+import tarfile
+my_tar = tarfile.open('my_tar.tar.gz')
+my_tar.extract('hello.txt','./my_folder')
+my_tar.close()
+this is an example of calling a shell command
+import subprocess
+process = subprocess.Popen(['echo', 'More output'],
+                     stdout=subprocess.PIPE, 
+                     stderr=subprocess.PIPE)
+stdout, stderr = process.communicate()
+stdout, stderr
